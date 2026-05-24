a9a93b1ba63ff5b56fa7ec7de3cc8c63a28d5603
ninja nithin <nithin3dev@gmail.com>
2026-05-24 21:56:31 +0530

    feat(render): clamp entity bounds, use worldEntity2Screen

--- render.c
+++ render.c
@@ -1,21 +1,38 @@
 #include "render.h"
+#include "object.h"
 #include "world.h"
 #include "camera.h"
+#include <stdlib.h>
 
 void render(SDL_Window *window, SDL_Renderer *renderer) {
+	if (cameraPos.w == '\0' || cameraPos.h == '\0') {
+		SDL_Log("camera not initialized");
+		exit(1);
+	}
 	for (int i = 0; i < PLAYER_LEN; i++) {
 		if (player[i] == NULL)
 			continue;
 		Player *p = player[i];
 		if (p->x >= cameraPos.x && p->x < cameraPos.x + cameraPos.w && p->y >= cameraPos.y && p->y < cameraPos.y + cameraPos.h) {
-			// TODO: render player
+			SDL_FRect r = worldEntity2Screen(window, (int)(p->x - p->r), (int)(p->y - p->r), p->r * 2, p->r * 2);
+			struct center c = { .x = r.x + r.w / 2, .y = r.y + r.h / 2 };
+			circle(renderer, c, (int)(r.w / 2), 1, 0, p->color);
 		}
 	}
 
-	for (int x = cameraPos.x; x < cameraPos.x + cameraPos.w; x++) {
-		for (int y = cameraPos.y; y < cameraPos.h; y++) {
+	int sx = cameraPos.x < 0 ? 0 : (int)cameraPos.x;
+	int sy = cameraPos.y < 0 ? 0 : (int)cameraPos.y;
+	int ex = cameraPos.x + cameraPos.w > ENTITY_LEN
+		? ENTITY_LEN : (int)(cameraPos.x + cameraPos.w);
+	int ey = cameraPos.y + cameraPos.h > ENTITY_LEN
+		? ENTITY_LEN : (int)(cameraPos.y + cameraPos.h);
+	for (int x = sx; x < ex; x++) {
+		for (int y = sy; y < ey; y++) {
 			if (ent[x][y] != NULL) {
-				// TODO: render entity
+				Entity *e = ent[x][y];
+				SDL_FRect r = worldEntity2Screen(window, x - 5, y - 5, 10, 10);
+				struct center c = { .x = r.x + r.w / 2, .y = r.y + r.h / 2 };
+				circle(renderer, c, (int)(r.w / 2), 1, 0, e->color);
 			}
 		}
 	}
