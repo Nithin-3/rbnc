b51cbb9307f77883054b022e4aa097f3b73e8dfb
ninja nithin <nithin3dev@gmail.com>
2026-05-26 17:10:04 +0530

    fix: send date as same type (Buffer)

--- serv.ts
+++ serv.ts
@@ -4,7 +4,7 @@ const wss = new WebSocketServer({ port: 3000 });
 
 wss.on('connection', (ws: WebSocket) => {
   ws.on('message', (msg: Buffer) => {
-    console.log("received:", msg.toString());
-    ws.send(msg.toString());
+    console.log("received:", msg);
+    ws.send(msg);
   });
 });
